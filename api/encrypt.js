6ff2df6bbfdb6b836e85a44112d6da1c987a0dfc
oakfordrealtyau-cpu <oakfordrealtyau@gmail.com>
2026-03-03 21:45:08 +0800

    Update encrypt.js

--- api/encrypt.js
+++ api/encrypt.js
@@ -1,7 +1,7 @@
 // api/encrypt.js
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
@@ -31,3 +31,4 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: err.message });
   }
 }
+
