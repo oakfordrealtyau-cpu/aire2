c5a84bf1f6b196a82e6080a82a53e53eda93174b
oakfordrealtyau-cpu <oakfordrealtyau@gmail.com>
2026-03-03 19:55:48 +0800

    Delete server directory

--- server/services/auditLog.js
+++
@@ -1,8 +0,0 @@
-import db from '../config/db.js';
-
-export default async function auditLog({ userId, action, oldValue, newValue, ip, userAgent }) {
-  await db.query(
-    'INSERT INTO audit_logs (user_id, action, old_value, new_value, ip, user_agent, created_at) VALUES (?, ?, ?, ?, ?, ?, NOW())',
-    [userId, action, JSON.stringify(oldValue), JSON.stringify(newValue), ip, userAgent]
-  );
-}
