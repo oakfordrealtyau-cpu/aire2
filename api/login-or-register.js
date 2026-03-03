e1f511fa3a7bc8376ea0806b63b8c8476a40b9da
oakfordrealtyau-cpu <oakfordrealtyau@gmail.com>
2026-03-03 21:46:01 +0800

    Update login-or-register.js

--- api/login-or-register.js
+++ api/login-or-register.js
@@ -12,7 +12,7 @@ function hmacSha256(message, secret) {
 export default async function handler(req, res) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
@@ -53,4 +53,5 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: "Login failed", details: err.message });
   }
+
 }
